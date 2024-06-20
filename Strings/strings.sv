8f8d139aa27db1e9064129336d975c2dcfbf44cb
Hoàng Bảo Long <105693455+106210049@users.noreply.github.com>
2024-06-20 18:36:44 +0700

    Update strings.sv

--- Strings/strings.sv
+++ Strings/strings.sv
@@ -100,7 +100,7 @@ module string_basic_methods;
     $display("s1=%0s s2=%0s difference=%0d",s1,s2,s1.icompare(s2)); //case insensitive
 //     icompare: không phân biệt chữ cái viết hoa hay thường
 //     compare: phân biệt hoa và thường
-//     compare và icompare trả về 0 nếu bằng nhau và -1 nếu không bằng nhau
+//     compare và icompare trả về 0 nếu bằng nhau và >0 khi kí tự bên trái có ASCII nhỏ hơn bên phải và ngược lại
   end
 endmodule
 //----------------------------------------------------------------------
