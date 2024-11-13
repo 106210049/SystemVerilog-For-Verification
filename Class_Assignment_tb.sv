401267dfcb2d11aa539a9229588fee247ad4de7d
Hoàng Bảo Long <105693455+106210049@users.noreply.github.com>
2024-11-13 13:12:16 +0700

    Create Class_Assignment_tb.sv

---
+++ Class_Assignment_tb.sv
@@ -0,0 +1,21 @@
+// Code your testbench here
+// or browse Examples
+class transaction;
+  bit [31:0] data;
+endclass
+
+module classAssignment;
+  transaction tr1,tr2;
+  initial begin
+    
+    tr1.new();
+    tr1.data=5;
+    tr2=tr1;
+    $display("tr1.data= %0h",tr1.data);
+    $display("tr2.data= %0h",tr2.data);
+
+    tr2.data=10;
+    $display("tr1.data= %0h",tr1.data);
+    $display("tr2.data= %0h",tr2.data);
+  end
+endmodule
